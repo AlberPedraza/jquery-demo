078d37f5ef6f9794cbece58d649f37c037fa5405
Alber Pedraza <alberpedrazaes@gmail.com>
2017-10-25 01:28:40 +0200

    add repository jquery demo

---
+++ script.js
@@ -0,0 +1,5 @@
+$( document ).ready(function(){
+  $("#menu ul li:first").addClass("selected");
+  $("section article:nth-child(2)").addClass("middle");
+  $(".list-item:odd").addClass("highlighted");
+});
